8a0a0b757196d7375f9d153148871c21de42cbaf
maureen <maureen.chelangat@student.moringaschool.com>
2023-10-10 07:14:22 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskInput = document.getElementById("new-task-description");
+  const createTaskForm = document.getElementById("create-task-form");
+  const tasksList = document.getElementById("tasks");
+
+  taskInput.addEventListener("input", (event) => {
+    taskText = event.target.value;
+  });
+
+  createTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Create a new list item with the task text
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskText;
+
+    // Append the task item to the tasks list
+    tasksList.appendChild(taskItem);
+
+    // Clear the input field
+    taskInput.value = "";
+  });
 });
